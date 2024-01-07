e5b78747c0894173b5d2fefe563494da314390a2
xsrf <git@xsrf.de>
2024-01-07 19:01:09 +0100

    change config for bootloader

--- lib/rv003usb/usb_config.h
+++ lib/rv003usb/usb_config.h
@@ -1,20 +1,27 @@
 #ifndef _USB_CONFIG_H
 #define _USB_CONFIG_H
 
-//Defines the number of endpoints for this device. (Always add one for EP0). For two EPs, this should be 3.
-#define ENDPOINTS 3
+//Defines the number of endpoints for this device. (Always add one for EP0). For two EPs, this should be 3.  For one, 2.
+#define ENDPOINTS 2
 
-#define USB_DM 3
-#define USB_DP 4
-#define USB_DPU 5
-#define USB_PORT D
+/*	
+	CH32V003FUN DevBoard:
+	PD3 D+
+	PD4 D-
+	PD5 D-_PU
+
+	CH32V003J4M6:
+	PC1 D+
+	PC2 D-
+	PC4 D-_PU
+*/
+
+#define USB_DM 3 // USB_DM is the physical USB D+ Pin!
+#define USB_DP 4 // USB_DP is the physical USB D- Pin!
+#define USB_DPU 5 // USB_DPU feeds the 1.5K Pull-Up on USB D- Pin! Comment out if not used / tied to 3V3!
+#define USB_PORT D // Pins on PORT A, C or D
 
 #define RV003USB_OPTIMIZE_FLASH 1
-#define RV003USB_EVENT_DEBUGGING 0
-#define RV003USB_HANDLE_IN_REQUEST 1
-#define RV003USB_OTHER_CONTROL 0
-#define RV003USB_HANDLE_USER_DATA 0
-#define RV003USB_HID_FEATURES 0
 
 #ifndef __ASSEMBLER__
 
@@ -30,8 +37,8 @@ static const uint8_t device_descriptor[] = {
 	0x0, //Device Subclass
 	0x0, //Device Protocol  (000 = use config descriptor)
 	0x08, //Max packet size for EP0 (This has to be 8 because of the USB Low-Speed Standard)
-	0x09, 0x12, //ID Vendor
-	0x03, 0xc0, //ID Product
+	0x09, 0x12, //ID Vendor   //TODO: register this in http://pid.codes/howto/ or somewhere.
+	0x03, 0xb0, //ID Product
 	0x02, 0x00, //ID Rev
 	1, //Manufacturer string
 	2, //Product string
@@ -39,106 +46,44 @@ static const uint8_t device_descriptor[] = {
 	1, //Max number of configurations
 };
 
-static const uint8_t mouse_hid_desc[] = {  //From http://eleccelerator.com/tutorial-about-usb-hid-report-descriptors/
-	HID_USAGE_PAGE( HID_USAGE_PAGE_DESKTOP ),         // USAGE_PAGE (Generic Desktop)
-	HID_USAGE( HID_USAGE_DESKTOP_TABLET_PC_SYSTEM ),  // USAGE (Mouse), Ok actually a tablet but we apply relative motion!
-	HID_COLLECTION ( HID_COLLECTION_APPLICATION ),    // COLLECTION (Application)
-		HID_USAGE( HID_USAGE_DESKTOP_POINTER ),       //   USAGE (Pointer)
-		HID_COLLECTION ( HID_COLLECTION_PHYSICAL ),   //   COLLECTION (Physical)
-			HID_USAGE_PAGE( HID_USAGE_PAGE_BUTTON ),  //     USAGE_PAGE (Button)
-			HID_USAGE_MIN( 1 ),                       //     USAGE_MINIMUM (Button 1)
-			HID_USAGE_MAX( 3 ),                       //     USAGE_MAXIMUM (Button 3)
-			HID_LOGICAL_MIN( 0 ),                     //     LOGICAL_MINIMUM (0)
-			HID_LOGICAL_MAX( 1 ),                     //     LOGICAL_MAXIMUM (1)
-			HID_REPORT_COUNT( 3 ),                    //     REPORT_COUNT (3)
-			HID_REPORT_SIZE( 1 ),                     //     REPORT_SIZE (1)
-			HID_INPUT( 0x02 ),                        //     INPUT (Data,Var,Abs)
-			HID_REPORT_COUNT( 1 ),                    //     REPORT_COUNT (1)
-			HID_REPORT_SIZE( 5 ),                     //     REPORT_SIZE (5)
-			HID_INPUT( 0x03 ),                        //     INPUT (Cnst,Var,Abs)
-			HID_USAGE_PAGE( HID_USAGE_PAGE_DESKTOP ), //     USAGE_PAGE (Desktop)
-			HID_USAGE( HID_USAGE_DESKTOP_X ),         //     USAGE (X)
-			HID_USAGE( HID_USAGE_DESKTOP_Y ),         //     USAGE (Y)
-			HID_USAGE( HID_USAGE_DESKTOP_WHEEL ),     //     USAGE (Wheel)
-			HID_LOGICAL_MIN( 0x81 ),                  //     LOGICAL_MINIMUM -127
-			HID_LOGICAL_MAX( 0x7f ),                  //     LOGICAL_MAXIMUM (127)
-			HID_REPORT_SIZE( 8 ),                     //     REPORT_SIZE (8)
-			HID_REPORT_COUNT( 3 ),                    //     REPORT_COUNT (3)
-			HID_INPUT( 0x06 ),                        //     INPUT (Data,Var,Rel)
-		HID_COLLECTION_END,                           //   END_COLLECTION
-	HID_COLLECTION_END,                               // END_COLLECTIONs
 
-	// Tack this on to do custom HID commands.
-	HID_COLLECTION ( HID_COLLECTION_APPLICATION )                 ,
-		HID_REPORT_ID    ( 0xaa                                   )
-		HID_USAGE        ( 0xff              ) ,
-		HID_FEATURE      ( HID_DATA | HID_ARRAY | HID_ABSOLUTE    ) ,
-		HID_REPORT_COUNT ( 8 ) ,
-	HID_COLLECTION_END,
+static const uint8_t special_hid_desc[] = { 
+  HID_USAGE_PAGE ( HID_USAGE_PAGE_DESKTOP      )                 ,
+  HID_USAGE      ( 0xff  )                 , // Needed?
+  HID_COLLECTION ( HID_COLLECTION_APPLICATION )                 ,
+    HID_REPORT_SIZE ( 8 ),
+    HID_REPORT_COUNT ( 127 ) ,
+    HID_REPORT_ID    ( 0xaa                                   )
+    HID_USAGE        ( 0xff              ) ,
+    HID_FEATURE      ( HID_DATA | HID_ARRAY | HID_ABSOLUTE    ) ,
+  HID_COLLECTION_END
 };
 
-//From http://codeandlife.com/2012/06/18/usb-hid-keyboard-with-v-usb/
-static const uint8_t keyboard_hid_desc[] = {   /* USB report descriptor */
-	HID_USAGE_PAGE( HID_USAGE_PAGE_DESKTOP ),         // USAGE_PAGE (Generic Desktop)
-	HID_USAGE( HID_USAGE_DESKTOP_KEYBOARD ),          // USAGE (Keyboard)
-	HID_COLLECTION ( HID_COLLECTION_APPLICATION ),    // COLLECTION (Application)
-		HID_REPORT_SIZE( 1 ),                         //     REPORT_SIZE (8)
-		HID_REPORT_COUNT( 8 ),                        //     REPORT_COUNT (3)
-		HID_USAGE_PAGE( HID_USAGE_PAGE_KEYBOARD ),    //     USAGE_PAGE (Keyboard)(Key Codes)
-    	HID_USAGE_MIN( 0xe0 ),                        //     USAGE_MINIMUM (Keyboard LeftControl)(224)
-    	HID_USAGE_MAX( 0xe7 ),                        //     USAGE_MAXIMUM (Keyboard Right GUI)(231)
-		HID_LOGICAL_MIN( 0 ),                         //     LOGICAL_MINIMUM (0)
-		HID_LOGICAL_MAX( 1 ),                         //     LOGICAL_MAXIMUM (1)
-		HID_INPUT( 0x02 ),                            //     INPUT (Data,Var,Abs) ; Modifier byte
-		HID_REPORT_COUNT( 1 ),                        //     REPORT_COUNT (1)
-		HID_REPORT_SIZE( 8 ),                         //     REPORT_SIZE (8)
-		HID_INPUT( 0x03 ),                            //     INPUT (Cnst,Var,Abs) ; Reserved byte
-		HID_REPORT_COUNT( 5 ),                        //     REPORT_COUNT (5)
-		HID_REPORT_SIZE( 1 ),                         //     REPORT_SIZE (1)
-		HID_USAGE_PAGE( HID_USAGE_PAGE_LED ),         //     USAGE_PAGE (LEDs)
-    	HID_USAGE_MIN( 0x01 ),                        //     USAGE_MINIMUM (Num Lock)
-	    HID_USAGE_MAX( 0x05 ),                        //     USAGE_MAXIMUM (Kana)
-		HID_OUTPUT( 0x02 ),                           //     OUTPUT (Data,Var,Abs) ; LED report
-		HID_REPORT_COUNT( 1 ),                        //     REPORT_COUNT (1)
-		HID_REPORT_SIZE( 3 ),                         //     REPORT_SIZE (3)
-		HID_OUTPUT( 0x03 ),                           //     OUTPUT (Cnst,Var,Abs) ; LED report padding
-		HID_REPORT_COUNT( 6 ),                        //     REPORT_COUNT (6)
-		HID_REPORT_SIZE( 8 ),                         //     REPORT_SIZE (8)
-		HID_LOGICAL_MIN( 0 ),                         //     LOGICAL_MINIMUM (0)
-		HID_LOGICAL_MAX( 167 ),                       //     LOGICAL_MAXIMUM (167)  (Normally would be 101, but we want volume buttons)
-    	HID_USAGE_PAGE( HID_USAGE_PAGE_KEYBOARD ),     //     USAGE_PAGE (Keyboard)(Key Codes)
-    	HID_USAGE_MIN( 0x00 ),                        //     USAGE_MINIMUM (0)
-	    HID_USAGE_MAX( 167 ),                         //     USAGE_MAXIMUM (Keyboard Application)(101) (Now 167)
-	HID_INPUT( 0 ),                                   //   INPUT (Data,Ary,Abs)
-    HID_COLLECTION_END,                               // END_COLLECTION
-};
-
-//Ever wonder how you have more than 6 keys down at the same time on a USB keyboard?  It's easy. Enumerate two keyboards!
-// No, really, that's what some hardware manufacturers do.
 
 static const uint8_t config_descriptor[] = {  //Mostly stolen from a USB mouse I found.
 	// configuration descriptor, USB spec 9.6.3, page 264-266, Table 9-10
 	9, 					// bLength;
 	2,					// bDescriptorType;
-	0x3b, 0x00,			// wTotalLength 
-	0x02,					// bNumInterfaces (Normally 1)
+	0x22, 0x00,			// wTotalLength  	
+
+	//34, 0x00, //for just the one descriptor
+	
+	0x01,					// bNumInterfaces (Normally 1)  (If we need an additional HID interface add here)
 	0x01,					// bConfigurationValue
 	0x00,					// iConfiguration
 	0x80,					// bmAttributes (was 0xa0)
 	0x64,					// bMaxPower (200mA)
 
-	// This descriptor shows how to embed two HIDs, to build a composite HID device.
 
-
-	//Mouse
+	//HID THING
 	9,					// bLength
 	4,					// bDescriptorType
 	0,			// bInterfaceNumber (unused, would normally be used for HID)
 	0,					// bAlternateSetting
 	1,					// bNumEndpoints
 	0x03,					// bInterfaceClass (0x03 = HID)
-	0x01,					// bInterfaceSubClass
-	0x02,					// bInterfaceProtocol (Mouse)
+	0xff,					// bInterfaceSubClass
+	0xff,					// bInterfaceProtocol
 	0,					// iInterface
 
 	9,					// bLength
@@ -147,52 +92,29 @@ static const uint8_t config_descriptor[] = {  //Mostly stolen from a USB mouse I
 	0x00, //country code
 	0x01, //Num descriptors
 	0x22, //DescriptorType[0] (HID)
-	sizeof(mouse_hid_desc), 0x00,
+	sizeof(special_hid_desc), 0x00, //Descriptor length XXX This looks wrong!!!
 
 	7, //endpoint descriptor (For endpoint 1)
 	0x05, //Endpoint Descriptor (Must be 5)
 	0x81, //Endpoint Address
 	0x03, //Attributes
-	0x04,	0x00, //Size
-	10, //Interval (Number of milliseconds between polls)
-
-
-	//Keyboard  (It is unusual that this would be here)
-	9,					// bLength
-	4,					// bDescriptorType
-	1,			// bInterfaceNumber  = 1 instead of 0 -- well make it second.
-	0,					// bAlternateSetting
-	1,					// bNumEndpoints
-	0x03,					// bInterfaceClass (0x03 = HID)
-	0x01,					// bInterfaceSubClass
-	0x01,					// bInterfaceProtocol (??)
-	0,					// iInterface
-
-	9,					// bLength
-	0x21,					// bDescriptorType (HID)
-	0x10,0x01,		//bcd 1.1
-	0x00, //country code
-	0x01, //Num descriptors
-	0x22, //DescriptorType[0] (HID)
-	sizeof(keyboard_hid_desc), 0x00, 
-
-	7, //endpoint descriptor (For endpoint 1)
-	0x05, //Endpoint Descriptor (Must be 5)
-	0x82, //Endpoint Address
-	0x03, //Attributes
-	0x08,	0x00, //Size (8 bytes)
-	10, //Interval Number of milliseconds between polls.
+	0x08,	0x00, //Size
+	0xff, //Interval
 };
 
-#define STR_MANUFACTURER u"CNLohr"
-#define STR_PRODUCT      u"RV003USB"
-#define STR_SERIAL       u"000"
+
+//Ever wonder how you have more than 6 keys down at the same time on a USB keyboard?  It's easy. Enumerate two keyboards!
+
+#define STR_MANUFACTURER u"cnlohr"
+#define STR_PRODUCT      u"rv003usb"
+#define STR_SERIAL       u"NBTT" // Need to change to BOOT when we finally decide on a flashing mechanism.
 
 struct usb_string_descriptor_struct {
 	uint8_t bLength;
 	uint8_t bDescriptorType;
-	uint16_t wString[];
+	const uint16_t wString[];
 };
+
 const static struct usb_string_descriptor_struct string0 __attribute__((section(".rodata"))) = {
 	4,
 	3,
@@ -214,17 +136,17 @@ const static struct usb_string_descriptor_struct string3 __attribute__((section(
 	STR_SERIAL
 };
 
+
 // This table defines which descriptor data is sent for each specific
 // request from the host (in wValue and wIndex).
 const static struct descriptor_list_struct {
 	uint32_t	lIndexValue;
 	const uint8_t	*addr;
 	uint8_t		length;
-} descriptor_list[] = {
+} descriptor_list[] __attribute__((section(".rodata"))) = {
 	{0x00000100, device_descriptor, sizeof(device_descriptor)},
 	{0x00000200, config_descriptor, sizeof(config_descriptor)},
-	{0x00002200, mouse_hid_desc, sizeof(mouse_hid_desc)},
-	{0x00012200, keyboard_hid_desc, sizeof(keyboard_hid_desc)},
+	{0x00002200, special_hid_desc, sizeof(special_hid_desc)},
 	{0x00000300, (const uint8_t *)&string0, 4},
 	{0x04090301, (const uint8_t *)&string1, sizeof(STR_MANUFACTURER)},
 	{0x04090302, (const uint8_t *)&string2, sizeof(STR_PRODUCT)},	
@@ -234,6 +156,5 @@ const static struct descriptor_list_struct {
 
 #endif // INSTANCE_DESCRIPTORS
 
-#endif
-
+#endif // __ASSEMBLER__
 #endif 
---
+++ src/bootloader.c
@@ -0,0 +1,352 @@
+// Could be defined here, or in the processor defines.
+#define SYSTEM_CORE_CLOCK 48000000
+#define SYSTICK_USE_HCLK
+
+#include "ch32v003fun.h"
+#include <stdio.h>
+#include <string.h>
+
+#define INSTANCE_DESCRIPTORS
+#include "rv003usb.h"
+
+#define LOCAL_CONCAT(A, B) A##B
+#define LOCAL_EXP(A, B) LOCAL_CONCAT(A,B)
+
+// To use time debugging, enable thsi here, and RV003USB_DEBUG_TIMING in the .S
+// You must update in tandem
+//#define RV003USB_DEBUG_TIMING 1
+
+// If you don't want to automatically boot into the application ever,
+// comment out BOOTLOADER_TIMEOUT_USB and set this flag:
+//#define DISABLE_BOOTLOAD
+
+// Set this if you want the bootloader to keep the Port configuration untouched
+// Otherwise it will reset all Pins on the Port used to input; costs 8-16 Bytes
+//#define BOOTLOADER_KEEP_PORT_CFG
+
+// Timeout for bootloader after power-up, set to 0 to stay in bootloader forever
+// 75ms per unit; 67 ~= 5s
+#define BOOTLOADER_TIMEOUT_PWR 5
+
+// Timeout (reset) for bootloader once USB Host is detected, set to 0 to stay in bootloader forever
+// 75ms per unit; 0 costs 28 Bytes, >0 costs 48 Bytes; Comment out if not used
+//#define BOOTLOADER_TIMEOUT_USB 0
+
+// Timeout Timebase; Careful: Constant works out to a single lbu instruction. Changes may result in bigger code
+// -0x100000 = 302ms; -0x040000 = 75ms;
+#define BOOTLOADER_TIMEOUT_BASE -0x40000
+
+#define SCRATCHPAD_SIZE 128
+extern volatile int32_t runwordpad;
+extern uint8_t scratchpad[SCRATCHPAD_SIZE];
+
+#ifdef BOOTLOADER_TIMEOUT_USB
+volatile uint8_t reset_timeout = 0;
+#endif
+
+struct rv003usb_internal rv003usb_internal_data;
+
+// This is the data actually required for USB.
+uint8_t data_receptive;
+
+void SystemInit48HSIUNSAFE( void );
+
+int main()
+{
+	SysTick->CTLR = 5;
+
+	// Enable GPIOs, TIMERs
+	RCC->APB2PCENR = RCC_APB2Periph_GPIOD | RCC_APB2Periph_GPIOC | RCC_APB2Periph_TIM1 | RCC_APB2Periph_GPIOA  | RCC_APB2Periph_AFIO | RCC_APB2Periph_TIM1;
+
+#if defined( RV003USB_DEBUG_TIMING ) && RV003USB_DEBUG_TIMING
+	// GPIO C0 Push-Pull
+	GPIOC->CFGLR = (GPIO_Speed_50MHz | GPIO_CNF_OUT_PP)<<(4*0) |
+	               (GPIO_Speed_50MHz | GPIO_CNF_OUT_PP_AF)<<(4*3) | // PC3 = T1C3
+	               (GPIO_Speed_50MHz | GPIO_CNF_OUT_PP_AF)<<(4*4) | // PC4 = T1C4
+	               (GPIO_Speed_50MHz | GPIO_CNF_OUT_PP)<<(4*2);
+#endif
+
+#if defined( RV003USB_DEBUG_TIMING ) && RV003USB_DEBUG_TIMING
+	{
+		// PC4 is MCO (for watching timing)
+		GPIOC->CFGLR &= ~(GPIO_CFGLR_MODE4 | GPIO_CFGLR_CNF4);
+		GPIOC->CFGLR |= GPIO_CFGLR_CNF4_1 | GPIO_CFGLR_MODE4_0 | GPIO_CFGLR_MODE4_1;
+		RCC->CFGR0 = (RCC->CFGR0 & ~RCC_CFGR0_MCO) | RCC_CFGR0_MCO_SYSCLK;
+
+		// PWM is used for debug timing. 
+		TIM1->PSC = 0x0000;
+
+		// Auto Reload - sets period
+		TIM1->ATRLR = 0xffff;
+
+		// Reload immediately
+		TIM1->SWEVGR |= TIM_UG;
+
+		// Enable CH4 output, positive pol
+		TIM1->CCER |= TIM_CC3E | TIM_CC3NP;
+
+		// CH2 Mode is output, PWM1 (CC1S = 00, OC1M = 110)
+		TIM1->CHCTLR2 |= TIM_OC3M_2 | TIM_OC3M_1;
+
+		// Set the Capture Compare Register value to 50% initially
+		TIM1->CH3CVR = 2;
+		
+		// Enable TIM1 outputs
+		TIM1->BDTR |= TIM_MOE;
+		
+		// Enable TIM1
+		TIM1->CTLR1 |= TIM_CEN;
+	}
+#endif
+
+		
+	// GPIO setup.
+	LOCAL_EXP(GPIO,USB_PORT)->CFGLR = (
+#ifdef BOOTLOADER_KEEP_PORT_CFG
+		LOCAL_EXP(GPIO,USB_PORT)->CFGLR 
+#else
+		0x44444444 // reset value (all input)
+#endif
+		// Reset the USB Pins
+		& ~( 0xF<<(4*USB_DM) | 0xF<<(4*USB_DP) 
+		#ifdef USB_DPU
+			| 0xF<<(4*USB_DPU)
+		#endif
+		)
+	) |
+	// Configure the USB Pins
+#ifdef USB_DPU
+		(GPIO_Speed_50MHz | GPIO_CNF_OUT_PP)<<(4*USB_DPU) |
+#endif
+		(GPIO_Speed_In | GPIO_CNF_IN_PUPD)<<(4*USB_DM) | 
+		(GPIO_Speed_In | GPIO_CNF_IN_PUPD)<<(4*USB_DP);
+
+	// Configure USB_DP (D-) as an interrupt on falling edge.
+	AFIO->EXTICR = LOCAL_EXP(GPIO_PortSourceGPIO,USB_PORT)<<(USB_DP*2); // Configure EXTI interrupt for USB_DP
+	EXTI->INTENR = 1<<USB_DP; // Enable EXTI interrupt
+	EXTI->FTENR = 1<<USB_DP;  // Enable falling edge trigger for USB_DP (D-)
+
+#ifdef USB_DPU
+	// This drives USB_DPU (D- Pull-Up) high, which will tell the host that we are going on-bus.
+	LOCAL_EXP(GPIO,USB_PORT)->BSHR = 1<<USB_DPU;
+#endif
+
+	// enable interrupt
+	NVIC_EnableIRQ( EXTI7_0_IRQn );
+
+	// Bootloader timeout / localpad: 
+	// localpad counting up to 0 is used for timeout
+	// localpad transitioning from -1 to 0 boots user code
+	// localpad set to 0 disables timeout
+	// localpad counting down to 0 is used for executing code from scratchpad
+	int32_t localpad = BOOTLOADER_TIMEOUT_BASE * BOOTLOADER_TIMEOUT_PWR;
+	while(1)
+	{
+		if( localpad < 0 )
+		{
+			if( ++localpad == 0 )
+			{
+				// Boot to user program.
+#ifndef DISABLE_BOOTLOAD
+				FLASH->BOOT_MODEKEYR = FLASH_KEY1;
+				FLASH->BOOT_MODEKEYR = FLASH_KEY2;
+				FLASH->STATR = 0; // 1<<14 is zero, so, boot user code.
+				FLASH->CTLR = CR_LOCK_Set;
+				PFIC->SCTLR = 1<<31;
+#endif
+			}
+#ifdef BOOTLOADER_TIMEOUT_USB
+			// Bootloader timeout reset was requested after first USB Communication
+	#if BOOTLOADER_TIMEOUT_USB > 0
+			if(reset_timeout == 1) {
+				localpad = BOOTLOADER_TIMEOUT_BASE * BOOTLOADER_TIMEOUT_USB;
+				reset_timeout = 2; // prevent from triggering again
+			}
+	#else
+			if(reset_timeout) localpad = 0;
+	#endif
+#endif
+		}
+		if( localpad > 0 )
+		{
+			if( --localpad == 0 )
+			{
+				/* Scratchpad strucure:
+					4-bytes:		LONG( 0x000000aa )
+						... code (this is executed) (120 bytes)
+					4-bytes:        LONG( 0x1234abcd )
+
+					After the scratchpad is the runpad, its structure is:
+					4-bytes:   int32_t  if negative, how long to go before bootloading.  If 0, do nothing.  If positive, execute.
+						... 1kB of totally free space.
+				*/
+				typedef void (*setype)( uint32_t *, volatile int32_t * );
+				setype scratchexec = (setype)(scratchpad+4);
+				scratchexec( (uint32_t*)&scratchpad[0], &runwordpad );
+			}
+		}
+
+		uint32_t commandpad = runwordpad;
+		if( commandpad )
+		{
+			localpad = commandpad-1;
+			runwordpad = 0;
+		}
+	}
+}
+
+#define ENDPOINT0_SIZE 8 //Fixed for USB 1.1, Low Speed.
+
+void usb_pid_handle_in( uint32_t addr, uint8_t * data, uint32_t endp, uint32_t unused, struct rv003usb_internal * ist )
+{
+	ist->current_endpoint = endp;
+	struct usb_endpoint * e = &ist->eps[endp];
+
+	int tosend = 0;
+	const uint8_t * sendnow;
+	uint8_t sendtok = e->toggle_in?0b01001011:0b11000011;
+	
+	// Handle IN (sending data back to PC)
+	// Do this down here.
+	// We do this because we are required to have an in-endpoint.  We don't
+	// have to do anything with it, though.
+	if( endp ) //XXX TODO: This can be reworked - if it's anything other than "is_descriptor" then send nak.
+	{
+		usb_send_empty( sendtok );
+		return;
+	}
+
+	const uint8_t * tsend = e->opaque;
+
+	int offset = (e->count)<<3;
+	tosend = e->max_len - offset;
+	if( tosend > ENDPOINT0_SIZE ) tosend = ENDPOINT0_SIZE;
+	if( tosend < 0 ) tosend = 0;
+	sendnow = tsend + offset;
+
+	if( !tosend || !tsend )
+	{
+		usb_send_empty( sendtok );
+	}
+	else
+	{
+		usb_send_data( sendnow, tosend, 0, sendtok );
+	}
+	return;
+}
+
+void usb_pid_handle_data( uint32_t this_token, uint8_t * data, uint32_t which_data, uint32_t length, struct rv003usb_internal * ist )
+{
+#ifdef BOOTLOADER_TIMEOUT_USB
+	// Detect first USB communication in order to reset the Bootloader Timeout if configured
+	#if BOOTLOADER_TIMEOUT_USB > 0
+		if(reset_timeout == 0) reset_timeout = 1;
+	#else
+		reset_timeout = 1;
+	#endif
+#endif
+
+	//Received data from host.
+	int cep = ist->current_endpoint;
+	struct usb_endpoint * e = &ist->eps[cep];
+
+	// Alrady received this packet.
+	if( e->toggle_out != which_data )
+	{
+		goto just_ack;
+	}
+
+	if( cep == 0 )
+	{
+		e->toggle_out = !e->toggle_out;
+		if( ist->setup_request )
+		{
+			struct usb_urb * s = __builtin_assume_aligned( (struct usb_urb *)(data), 4 );
+
+			uint32_t wvi = s->lValueLSBIndexMSB;
+			//Send just a data packet.
+			e->count = 0;
+			e->opaque = 0;
+			ist->setup_request = 0;
+			e->max_len = 0;
+
+			if( s->wRequestTypeLSBRequestMSB == 0x01a1 )
+			{
+				// Class read request.
+				uint32_t wlen = s->wLength;
+				if( wlen > sizeof(scratchpad) ) wlen = SCRATCHPAD_SIZE;
+				// The host wants to read back from us.
+				e->max_len = wlen;
+				e->opaque = scratchpad;
+			}
+			else if( s->wRequestTypeLSBRequestMSB == 0x0921 )
+			{
+				// Class write request.
+				e->max_len = SCRATCHPAD_SIZE;
+				runwordpad = 1; //request stoppage.
+				e->opaque = scratchpad;
+			}
+			else if( (s->wRequestTypeLSBRequestMSB & 0xff80) == 0x0680 )
+			{
+				int i;
+				const struct descriptor_list_struct * dl;
+				for( i = 0; i < DESCRIPTOR_LIST_ENTRIES; i++ )
+				{
+					dl = &descriptor_list[i];
+					if( dl->lIndexValue == wvi )
+					{
+						// Send back descriptor.
+						e->opaque = (uint8_t*)dl->addr;
+						uint16_t swLen = s->wLength;
+						uint16_t elLen = dl->length;
+						e->max_len = (swLen < elLen)?swLen:elLen;
+						break;
+					}
+				}
+
+				goto just_ack;
+			}
+			else if( s->wRequestTypeLSBRequestMSB == 0x0500 )
+			{
+				//Set address.
+				ist->my_address = wvi;
+			}
+			else
+			{
+			}
+		}
+		else if( e->opaque )
+		{
+			// Continuing data.
+			uint8_t * start = &e->opaque[e->count<<3];
+			int l = length-3;
+			int i;
+			for( i = 0; i < l; i++ )
+				start[i] = data[i];//((intptr_t)data)>>(i*8);
+			e->count ++;
+
+			if( start + length - 3 >= scratchpad + SCRATCHPAD_SIZE )
+			{
+				// If the last 4 bytes are 0x1234abcd, then we can go!
+				uint32_t * last4 = (uint32_t*)(start + 4);					
+				if( *last4 == 0x1234abcd )
+				{
+					*last4 = 0;
+					runwordpad = 0x200; // Request exectution
+				}
+				e->opaque = 0;
+			}
+			// Allow user code to receive data.
+		}
+	}
+just_ack:
+	{
+		//Got the right data.  Acknowledge.
+		usb_send_data( 0, 0, 2, 0xD2 ); // Send ACK
+	}
+	return;
+}
+
+
+
+
